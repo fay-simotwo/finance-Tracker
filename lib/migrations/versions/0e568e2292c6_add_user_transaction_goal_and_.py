39f3821e0fbb5f73acb8e60caf23c0abd12eb51c
Moringa School <chepfaith18@gmail.com>
2023-09-06 19:12:02 +0300

    Edited the generated Alembic migration scripts

--- lib/migrations/versions/0e568e2292c6_add_user_transaction_goal_and_.py
+++ lib/migrations/versions/0e568e2292c6_add_user_transaction_goal_and_.py
@@ -5,20 +5,68 @@ Revises:
 Create Date: 2023-09-06 18:58:31.531110
 
 """
+# alembic/versions/001_initial_migration.py
+
 from alembic import op
 import sqlalchemy as sa
 
-
-# revision identifiers, used by Alembic.
-revision = '0e568e2292c6'
-down_revision = None
-branch_labels = None
-depends_on = None
-
-
 def upgrade():
-    pass
+    # Create the 'users' table
+    op.create_table('users',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('username', sa.String(length=255), nullable=False),
+        sa.Column('password', sa.String(length=255), nullable=False),
+        sa.Column('email', sa.String(length=255), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('username'),
+        sa.UniqueConstraint('email')
+    )
 
+    # Create the 'transactions' table
+    op.create_table('transactions',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('user_id', sa.Integer(), nullable=False),
+        sa.Column('date', sa.Date(), nullable=False),
+        sa.Column('description', sa.String(length=255), nullable=False),
+        sa.Column('amount', sa.Float(), nullable=False),
+        sa.Column('transaction_type', sa.String(length=255), nullable=False),
+        sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('id')
+    )
+
+    # Create the 'goals' table
+    op.create_table('goals',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('user_id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.String(length=255), nullable=False),
+        sa.Column('target_amount', sa.Float(), nullable=False),
+        sa.Column('current_amount', sa.Float(), nullable=False),
+        sa.Column('target_date', sa.Date()),
+        sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('id')
+    )
+
+    # Create the 'investments' table
+    op.create_table('investments',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('user_id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.String(length=255), nullable=False),
+        sa.Column('amount_invested', sa.Float(), nullable=False),
+        sa.Column('current_value', sa.Float(), nullable=False),
+        sa.Column('purchase_date', sa.Date(), nullable=False),
+        sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('id')
+    )
 
 def downgrade():
-    pass
+    # Drop the 'investments' table
+    op.drop_table('investments')
+
+    # Drop the 'goals' table
+    op.drop_table('goals')
+
+    # Drop the 'transactions' table
+    op.drop_table('transactions')
+
+    # Drop the 'users' table
+    op.drop_table('users')
