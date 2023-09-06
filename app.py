0e3086709bb1f7a6d27b72591ad38a8ca4ab187b
Moringa School <chepfaith18@gmail.com>
2023-09-06 19:38:50 +0300

    Create a database engine and a session

--- app.py
+++ app.py
@@ -2,3 +2,8 @@
 from models import User, Transaction, Goal, Investment
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+# Create a database engine and a session
+engine = create_engine('sqlite:///finance_tracker.db')
+Session = sessionmaker(bind=engine)
+session = Session()
