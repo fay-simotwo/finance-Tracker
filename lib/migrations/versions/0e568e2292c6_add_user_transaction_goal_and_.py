a99095c455f3c35d26efad88ad95965376dfb6f7
Moringa School <chepfaith18@gmail.com>
2023-09-06 19:04:20 +0300

    File Setup

---
+++ lib/migrations/versions/0e568e2292c6_add_user_transaction_goal_and_.py
@@ -0,0 +1,24 @@
+"""Add user, transaction, goal, and investment tables
+
+Revision ID: 0e568e2292c6
+Revises: 
+Create Date: 2023-09-06 18:58:31.531110
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0e568e2292c6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
